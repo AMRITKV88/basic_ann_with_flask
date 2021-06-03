385f2abba824ef7e83d4f2071e6a743aa05a1b71
akverma8 <er.akverma8@gmail.com>
2021-06-03 20:21:44 +0530

    minor modifications

--- Dockerfile
+++ Dockerfile
@@ -10,4 +10,4 @@ COPY . /app
 
 RUN pip install --no-cache-dir -r requirements.txt
 
-CMD [ "python", "run.py" ]
+CMD [ "python", "-u", "run.py" ]
--- run.py
+++ run.py
@@ -6,8 +6,11 @@ app = Flask(__name__)
 @app.route("/")
 def index():
 
+    print("\n\nRelaxxxx --> The model is training....!")
     test_accuracy = run_ann()
     print("\n\n --> Testing accuracy : ", test_accuracy, "\n\n")
+    response = {"Testing Accuracy": test_accuracy}
+    return response
     
 if __name__ == "__main__":
     app.run(debug=True,host='0.0.0.0',port=8080)
